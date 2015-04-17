d663a7207dc27f209d393504bce85a5d2146c9ce
Sean M. Snyder <sean.snyder@snhu.edu>
2015-04-16 20:46:14 -0400

    title application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,35 @@
 module ApplicationHelper
-  def markdown(text)
-      options = {
+    def markdown(text)
+        options = {
         filter_html:     true,
         hard_wrap:       true, 
         link_attributes: { rel: 'nofollow', target: "_blank" },
         space_after_headers: true, 
         fenced_code_blocks: true
-      }
+        }
 
-      extensions = {
+        extensions = {
         autolink:           true,
         superscript:        true,
         disable_indented_code_blocks: true
-      }
+        }
 
-      renderer = Redcarpet::Render::HTML.new(options)
-      markdown = Redcarpet::Markdown.new(renderer, extensions)
+        renderer = Redcarpet::Render::HTML.new(options)
+        markdown = Redcarpet::Markdown.new(renderer, extensions)
 
-      markdown.render(text).html_safe
+        markdown.render(text).html_safe
     end
-  
+
     def current_time
-      Time.now
+        Time.now
+    end
+    
+    def titleize(page_title = "")
+      base_title = "Snyder-Dev"
+      if page_title.empty?
+        base_title
+      else
+        "#{base_title} | #{page_title}"
+      end
     end
 end
