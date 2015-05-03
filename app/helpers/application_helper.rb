e8cfb3bb4958052e004a8e26e28be36224fa0425
Sean M. Snyder <sean.snyder@snhu.edu>
2015-05-02 22:56:15 -0400

    styling and sidenav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,7 +25,7 @@ module ApplicationHelper
   end
 
   def full_post_date(article)
-    article.created_at.strftime("%B %d, %G")
+    article.created_at.strftime("%B %d, %Y")
   end
 
   def titleize(page_title = "")
