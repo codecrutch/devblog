ffa674983f5db66f35b5ce97a6077ffd5d1730be
Sean M. Snyder <sean.snyder@snhu.edu>
2015-05-05 10:30:29 -0400

    css change, abstract syntaxhighlighing js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,9 +18,3 @@
 //= require_tree .
 
 $(function(){ $(document).foundation();});
-
-// pull all code and pre tags from ckeditor for syntax highlighting
-$(document).ready(function(){
-$("code").addClass("language-ruby");
-$("pre").addClass("language-ruby");
-});
---
+++ app/assets/javascripts/syntaxhighlighter.js
@@ -0,0 +1,5 @@
+// pull all code and pre tags from ckeditor for syntax highlighting
+$(document).ready(function(){
+$("code").addClass("language-ruby");
+$("pre").addClass("language-ruby");
+});
