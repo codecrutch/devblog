f5464a37ab9eedaf12c77f63d84d024b0e6238d5
Sean M. Snyder <sean.snyder@snhu.edu>
2015-12-05 23:38:02 -0500

    disqus

--- app/assets/javascripts/disqus.js
+++ app/assets/javascripts/disqus.js
@@ -1,12 +1,21 @@
+<script>
+/**
+* RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+* LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+*/
+/*
+var disqus_config = function () {
+this.page.url = PAGE_URL; // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.async = true;
 
-    /* * * CONFIGURATION VARIABLES * * */
-    var disqus_shortname = 'snyder-dev';
-    
-    /* * * DON'T EDIT BELOW THIS LINE * * */
-    (function () {
-        var s = document.createElement('script'); s.async = true;
-        var loc = document.getElementById('disqus_thread');
-        s.type = 'text/javascript';
-        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
-        loc.appendChild(s);
-    }());
+s.src = '//snyder-dev.disqus.com/embed.js';
+
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
