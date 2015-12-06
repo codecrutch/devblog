a88180b869555832006cd61b0391f66ace730ed1
Sean M. Snyder <sean.snyder@snhu.edu>
2015-12-05 20:38:27 -0500

    move helper to proper location

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,10 +7,6 @@ module ApplicationHelper
     Date.now
   end
 
-  def full_post_date(article)
-    article.created_at.strftime("%B %d, %Y")
-  end
-
   def titleize(page_title = '')
     base_title = 'Snyder-Dev'
     if page_title.empty?
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,4 +1,7 @@
 # Article helpers
 
 module ArticlesHelper
+  def full_post_date(article)
+    article.created_at.strftime("%B %d, %Y")
+  end
 end
