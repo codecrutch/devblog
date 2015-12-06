76f836cef68e7017f385ca6928f899c3428bac17
Sean M. Snyder <sean.snyder@snhu.edu>
2015-12-05 23:20:20 -0500

    typo

--- app/assets/javascripts/disqus.js
+++ app/assets/javascripts/disqus.js
@@ -5,7 +5,7 @@
     /* * * DON'T EDIT BELOW THIS LINE * * */
     (function () {
         var s = document.createElement('script'); s.async = true;
-        var loc = document.getElementbyId('disqus_thread');
+        var loc = document.getElementById('disqus_thread');
         s.type = 'text/javascript';
         s.src = '//' + disqus_shortname + '.disqus.com/count.js';
         loc.appendChild(s);
