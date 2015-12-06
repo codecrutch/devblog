ed1b6cc8721719d331d75402c5ee424a5ac94f85
Sean M. Snyder <sean.snyder@snhu.edu>
2015-12-05 23:15:57 -0500

    disqus

--- app/assets/javascripts/disqus.js
+++ app/assets/javascripts/disqus.js
@@ -5,9 +5,8 @@
     /* * * DON'T EDIT BELOW THIS LINE * * */
     (function () {
         var s = document.createElement('script'); s.async = true;
-        var head = document.getElementsByTagName('head')[0];
-    	var body = document.getElementsByTagName('body')[0];
+        var loc = document.getElementbyId('disqus_thread');
         s.type = 'text/javascript';
         s.src = '//' + disqus_shortname + '.disqus.com/count.js';
-        (head || body).appendChild(s);
+        loc.appendChild(s);
     }());
